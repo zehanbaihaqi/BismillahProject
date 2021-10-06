196e0b55446156498e35c6f64bcfa4e62c79edce
Zehan Baihaqi <44484979+zehanbaihaqi@users.noreply.github.com>
2021-10-06 10:17:39 +0700

    firts commit
    commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+// import BlogList from "./BlogList";
+// import useFetch from "./useFetch";
+
+const Home = () => {
+ 
+  
+  return (
+    <div className="home">
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
